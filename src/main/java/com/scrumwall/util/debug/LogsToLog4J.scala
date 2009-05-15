5291fb19988abe830243d068e6da7d535f10032b
Heiti Allak <ristee@smail.ee>
2009-05-15 20:13:56 +0000

    Fixed the bug that error messages are only logged when debug is enabled.

--- src/main/java/com/scrumwall/util/debug/LogsToLog4J.scala
+++ src/main/java/com/scrumwall/util/debug/LogsToLog4J.scala
@@ -7,27 +7,28 @@ import java.lang.Class
 trait LogsToLog4J {
 	
 	private val toLog = Logger.getLogger(this.getClass)
-   
+    
 	def debug(message: Any) = {
-	  this.log( toLog.debug(_: Any), message ) 
+	  this.log(toLog isDebugEnabled,  toLog.debug(_: Any), message ) 
 	}
  
 	def debug(message: Any, exception: Exception) = {
-	  this.log( toLog debug(_: Any, _: Exception), message, exception )
+	  this.log(toLog isDebugEnabled, toLog debug(_: Any, _: Exception), message, exception )
 	}
  
 	def error(message: Any) = {
-	  this.log( toLog error(_: Any), message);
+	  this.log(true, toLog error(_: Any), message);
 	}
  
-	private def log(logMethod: Any => Unit, message: => Any) = {
-	  if( toLog.isDebugEnabled ) {
+	private def log(checkMethod: Boolean, logMethod: Any => Unit, message: => Any) = {
+	  if( checkMethod ) {
 		  logMethod( message )
 	  }
 	}
  
-	private def log(logMethod: (Any, Exception) => Unit, message: => Any, exception: Exception) = {
-		if( toLog.isDebugEnabled ) {
+	private def log(checkMethod: Boolean, logMethod: (Any, Exception) => Unit, 
+                 	message: => Any, exception: Exception) = {
+		if( checkMethod ) {
 			logMethod( message, exception )
 		}
 	}
