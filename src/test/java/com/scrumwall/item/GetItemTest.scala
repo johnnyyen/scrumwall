fe3b84842a398cd300aa232c3dbd17b15ca45efe
Heiti Allak <ristee@smail.ee>
2009-05-19 20:24:58 +0000

    Spring TestContext working in a testcase

--- src/test/java/com/scrumwall/item/GetItemTest.scala
+++ src/test/java/com/scrumwall/item/GetItemTest.scala
@@ -12,24 +12,27 @@ import org.junit.runner.RunWith
 import org.scalatest.tools.Runner
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.AbstractSpringContextTests
+import org.springframework.beans.factory.annotation.Autowired
 
 @RunWith(classOf[org.springframework.test.context.junit4.SpringJUnit4ClassRunner])
-@ContextConfiguration(locations={"test-context.xml"})
+@ContextConfiguration{val locations = Array("classpath:applicationContext.xml", "classpath:test-ds.xml"), val inheritLocations = false}
 class GetItemTest extends TestCase with FunSuite{
 
   var itemDao: ItemDao = null
   
+  @Autowired def setItemDao(itemDao: ItemDao) = { this.itemDao = itemDao }
+  
   @Test def testSomething() {
-    assert(1==1)
+	val request = new MockHttpServletRequest
+	val response = new MockHttpServletResponse
+	
+	val controller = new WelcomeController(itemDao) 
+	val mv = controller.handleRequestInternal(request, response)
+ 
+	assert(mv != null)
   }
   
   test("You should not be able to send in strings") {
-    val request = new MockHttpServletRequest
-    val response = new MockHttpServletResponse
-    
-    val controller = new WelcomeController(itemDao) 
-    val mv = controller.handleRequestInternal(request, response)
-    assert(mv != null)
   }
   
 }
---
+++ src/test/java/com/scrumwall/item/ItemDaoTest.scala
@@ -0,0 +1,14 @@
+package com.scrumwall.item
+
+import org.scalatest.junit.JUnit3Suite
+import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests
+import junit.framework.TestCase
+import org.junit.Test
+
+class ItemDaoTest extends TestCase {
+
+  @Test def testDoSomething() = {
+    assert( 1==1 )
+  }
+  
+}
