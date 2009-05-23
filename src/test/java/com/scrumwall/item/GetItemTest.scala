6b9e7ae7ace0e3c2cc6c6073626a767b2dcd90dc
Heiti Allak <ristee@smail.ee>
2009-05-23 11:36:56 +0000

    removed unused imports

--- src/test/java/com/scrumwall/item/GetItemTest.scala
+++ src/test/java/com/scrumwall/item/GetItemTest.scala
@@ -2,17 +2,13 @@ package com.scrumwall.item
 
 import com.scrumwall.helper.BaseTestCase
 import org.scalatest.FunSuite
-import org.scalatest.tools.Runner
 import org.springframework.mock.web.MockHttpServletRequest
 import org.springframework.mock.web.MockHttpServletResponse
-import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.web.ModelAndViewAssert
 import org.springframework.beans.factory.annotation.Autowired
 import com.scrumwall.controller.WelcomeController
 import com.scrumwall.dao.item.ItemDao
-import junit.framework.TestCase
 import org.junit.Test
-import org.junit.runner.RunWith
 
 class GetItemTest extends BaseTestCase {
 
