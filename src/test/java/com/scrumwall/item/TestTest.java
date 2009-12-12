6f7917fd15f83ab30d802dd732822287663dd52a
Silver Juurik <silverjuurik@gmail.com>
2009-12-12 18:37:02 +0000

    adding a dummy test case for selenium, just to check if it works in CI

--- src/test/java/com/scrumwall/item/TestTest.java
+++ src/test/java/com/scrumwall/item/TestTest.java
@@ -1,14 +1,17 @@
 package com.scrumwall.item;
 
+import com.thoughtworks.selenium.SeleneseTestCase;
 import junit.framework.TestCase;
 
-public class TestTest extends TestCase {//extends SeleneseTestCase {
+public class TestTest extends SeleneseTestCase {
 	public void setUp() throws Exception {
-		//setUp("http://localhost:8080/", "*chrome");
+        super.setUp();
+		setUp("http://localhost:4444/", "*firefox");
 	}
 	
 	public void testUntitled() throws Exception {
-//		selenium.open("/scrumwall/Layout.form#");
+		selenium.open("/scrumwall/Layout.form#");
+
 		//selenium.click("//div[@id='col.15']/div[1]/a");
 		//selenium.click("newColumnButton");
 //		selenium.click("//div[@id='col.14']/div[1]/a");
